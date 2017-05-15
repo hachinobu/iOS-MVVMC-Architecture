19ddf2caf98db5c6c162b6ab280534d091df9256
hachinobu <hachinobu@gmail.com>
2017-05-15 19:55:23 +0900

    WIP Create View

---
+++ iOS-MVVMC-Architecture/View/TableViewCell/TimeLineTweetCell.swift
@@ -0,0 +1,21 @@
+//
+//  TimeLineTweetCell.swift
+//  iOS-MVVMC-Architecture
+//
+//  Created by Nishinobu.Takahiro on 2017/05/15.
+//  Copyright © 2017年 hachinobu. All rights reserved.
+//
+
+import UIKit
+
+class TimeLineTweetCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+
+}
