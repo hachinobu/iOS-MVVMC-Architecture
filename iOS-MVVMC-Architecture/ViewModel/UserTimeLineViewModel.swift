3d036130292fbfdb1807ca830742e70d07f48097
hachinobu <hachinobu@gmail.com>
2017-06-03 15:43:12 +0900

    Clean up UserTimeLineViewModel

--- iOS-MVVMC-Architecture/ViewModel/UserTimeLineViewModel.swift
+++ iOS-MVVMC-Architecture/ViewModel/UserTimeLineViewModel.swift
@@ -26,32 +26,15 @@ final class UserTimeLineViewModel: TimeLineViewModel {
     
     private let list: Variable<[Tweet]?> = Variable(nil)
     lazy var tweets: Driver<[TimeLineCellViewModel]> = {
-        
         return self.list.asObservable()
-            .withLatestFrom(self.fetchAction.inputs) { $0 }
-            .scan([Tweet]()) { currentElements, nextFetchInfo -> [Tweet] in
-                
-                let results = nextFetchInfo.0
-                let id: Int64? = nextFetchInfo.1
-                
-                if currentElements.isEmpty || id == nil {
-                    return results!
-                }
-                
-                if let id = id, let firstId = results?.first?.id, id > firstId {
-                    return currentElements + results!
-                }
-                
-                return currentElements
-                
-            }
+            .filter { $0 != nil }
+            .map { $0! }
             .map { try $0.map(HomeTimeLineViewModelTranslator()) }
             .asDriver(onErrorJustReturn: [])
-        
     }()
     
     lazy var error: Driver<Error> = {
-        self.fetchAction.errors.asDriver(onErrorDriveWith: .empty())
+        self.fetchTimeLineAction.errors.asDriver(onErrorDriveWith: .empty())
             .flatMap { error -> Driver<Error> in
                 switch error {
                 case .underlyingError(let error):
@@ -67,10 +50,10 @@ final class UserTimeLineViewModel: TimeLineViewModel {
     }()
     
     lazy var loadingIndicatorAnimation: Driver<Bool> = {
-        return self.fetchAction.executing.shareReplayLatestWhileConnected().asDriver(onErrorJustReturn: false)
+        return self.fetchTimeLineAction.executing.shareReplayLatestWhileConnected().asDriver(onErrorJustReturn: false)
     }()
     
-    private let fetchAction: Action<Int64?, [Tweet]>
+    private let fetchTimeLineAction: Action<Int64?, [Tweet]>
     private let authTwitter = AuthenticateTwitter.sharedInstance
     
     private let fetchUserAction: Action<Void, User>
@@ -80,7 +63,7 @@ final class UserTimeLineViewModel: TimeLineViewModel {
     init(userId: String, viewWillAppear: Driver<Void>) {
         
         let account = authTwitter.currentAccount.asObservable()
-        fetchAction = Action { sinceId in
+        fetchTimeLineAction = Action { sinceId in
             var parameters = ["user_id": userId]
             if let sinceId = sinceId {
                 parameters["max_id"] = sinceId.description
@@ -97,34 +80,38 @@ final class UserTimeLineViewModel: TimeLineViewModel {
                 .shareReplayLatestWhileConnected()
         }
         
-//        viewWillAppear.asObservable()
-//            .take(1)
-//            .map { nil }
-//            .subscribe(onNext: { [weak self] id in
-//                self?.fetchAction.execute(id)
-//            }).addDisposableTo(bag)
-        
-        let connect = Observable.zip(fetchAction.elements, fetchUserAction.elements)
+        let connect = Observable.zip(fetchTimeLineAction.elements, fetchUserAction.elements)
+            .withLatestFrom(fetchTimeLineAction.inputs) { $0 }
+            .filter { $0.1 == nil }
             .shareReplayLatestWhileConnected()
-        connect.map { $0.0 }.bind(to: list).addDisposableTo(bag)
-        connect.map { $0.1 }.bind(to: user).addDisposableTo(bag)
+        connect.map { $0.0.0 }.bind(to: list).addDisposableTo(bag)
+        connect.map { $0.0.1 }.bind(to: user).addDisposableTo(bag)
         
-        viewWillAppear.asObservable().take(1).subscribe(onNext: { _ in
-            self.fetchAction.execute(nil)
-            self.fetchUserAction.execute(())
+        viewWillAppear.asObservable().take(1).subscribe(onNext: { [weak self] _ in
+            self?.fetchTimeLineAction.execute(nil)
+            self?.fetchUserAction.execute(())
         }).addDisposableTo(bag)
+        
+        fetchTimeLineAction.elements.withLatestFrom(fetchTimeLineAction.inputs) { $0 }
+            .filter { $0.1 != nil }
+            .withLatestFrom(list.asObservable()) { (infos, currents) -> [Tweet]? in
+            if let lastId = currents?.last?.id, let firstId = infos.0.first?.id, lastId > firstId {
+                return currents! + infos.0
+            }
+            return infos.0
+        }.bind(to: list).addDisposableTo(bag)
         
     }
     
     func bindRefresh(refresh: Driver<Void>) {
         
         refresh.asObservable()
-            .withLatestFrom(loadingIndicatorAnimation.asObservable()) { (_, isRefresh) -> Bool in
-                return isRefresh
-            }.filter { !$0 }
-            .map { _ in return nil }
-            .bind(to: fetchAction.inputs)
-            .addDisposableTo(bag)
+            .withLatestFrom(loadingIndicatorAnimation.asObservable())
+            .filter { !$0 }
+            .subscribe(onNext: { [weak self] _ in
+                self?.fetchTimeLineAction.execute(nil)
+                self?.fetchUserAction.execute(())
+            }).addDisposableTo(bag)
         
     }
     
@@ -133,13 +120,13 @@ final class UserTimeLineViewModel: TimeLineViewModel {
         reachedBottom.asObservable()
             .withLatestFrom(loadingIndicatorAnimation.asObservable()) { $0.1 }
             .filter { !$0 }
-            .withLatestFrom(fetchAction.elements) { $0.1.last }
+            .withLatestFrom(fetchTimeLineAction.elements) { $0.1.last }
             .filter { $0 != nil }
             .map { tweet -> Int64 in
                 let id = tweet!.id - 1
                 return id
             }
-            .bind(to: fetchAction.inputs)
+            .bind(to: fetchTimeLineAction.inputs)
             .addDisposableTo(bag)
         
     }
